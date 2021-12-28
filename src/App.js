aca4614cb1905baf7a7c989b35a5d508aae2fdee
Mikah Trent <mtrent@M433213.local>
2021-12-28 11:19:56 -0600

    init firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBFZilx1RVdwEiLGEJYoVPWEy-ca9frAE0",
+  authDomain: "corp-site-9f14c.firebaseapp.com",
+  projectId: "corp-site-9f14c",
+  storageBucket: "corp-site-9f14c.appspot.com",
+  messagingSenderId: "11607933514",
+  appId: "1:11607933514:web:d17240b3661183a42adcb3",
+  measurementId: "G-9F0HRSPDYC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 function App() {
   return (
